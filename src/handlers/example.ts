95cb652b93f40639e0ee9d66c7e1fc669fb83053
HollyGrahamPlank <hollygrahamplank@gmail.com>
2023-07-19 16:57:29 -0500

    Added auto formatting with prettier

--- src/handlers/example.ts
+++ src/handlers/example.ts
@@ -1,17 +1,37 @@
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Context } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+  Context,
+} from 'aws-lambda';
 
-async function handlerValidation(event: APIGatewayProxyEvent, context: Context) {
-    // ...do nothing
-    return {};
+/**
+ * Validation for this handler function.
+ * @param event The AWS Event information for this event
+ * @param context The AWS Lambda execution context for this event
+ * @returns relevant info once the handler passes verification.
+ */
+async function handlerValidation(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  event: APIGatewayProxyEvent,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  context: Context,
+) {
+  // ...do nothing
 }
 
-export const handler: APIGatewayProxyHandler = async (event, context): Promise<APIGatewayProxyResult> => {
-    const { } = handlerValidation(event, context);
+export const handler: APIGatewayProxyHandler = async (
+  event,
+  context,
+): Promise<APIGatewayProxyResult> => {
+  handlerValidation(event, context);
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Hello World!"
-        })
-    };
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World!',
+    }),
+  };
+};
+
+export default handler;
