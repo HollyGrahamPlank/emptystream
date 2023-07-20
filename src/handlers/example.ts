52164d3296223027298837526f65ed35b7227381
HollyGrahamPlank <hollygrahamplank@gmail.com>
2023-07-20 12:22:34 -0500

    Documented example lambda handler

--- src/handlers/example.ts
+++ src/handlers/example.ts
@@ -5,6 +5,12 @@ import {
   Context,
 } from "aws-lambda";
 
+/** How the JSON that the handler returns should be formatted. */
+interface IHandlerOutput {
+  /** The message to return. In this example, it's always "Hello World!" */
+  message: string;
+}
+
 /**
  * Validation for this handler function.
  *
@@ -16,17 +22,23 @@ async function handlerValidation(event: APIGatewayProxyEvent, context: Context) 
   // ...do nothing
 }
 
+/**
+ * An example Lambda Handler. Simply returns "Hello World!" as json.
+ *
+ * @param event Info about the API Gateway event that triggered this lambda.
+ * @param context Context regarding this lambda call
+ * @returns APIGatewayProxyResult
+ */
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> => {
   handlerValidation(event, context);
 
+  const response: IHandlerOutput = { message: "Hello World!" };
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World!",
-    }),
+    body: JSON.stringify(response),
   };
 };
 
