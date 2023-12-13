c7be7a2769bafffa7f033bb66b1c002bd6a58f88
HollyGrahamPlank <hollygrahamplank@gmail.com>
2023-12-13 15:32:56 -0600

    Splitter can download transmission source

---
+++ containers/transmissionSplitter/src/downloadTransmissionSource.ts
@@ -0,0 +1,66 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import * as fs from "fs";
+import { Readable } from "stream";
+
+//
+//  Local Functions
+//
+
+/** @returns A new S3 client to use. */
+function createS3Client(): S3Client {
+  // This is a function because in the future we may want to configure this client
+  // specifically depending on the runtime environment, such as if we are running
+  // this function locally.
+  return new S3Client({});
+}
+
+/**
+ * Grab a single file from an S3 Bucket, using a new client.
+ *
+ * @param bucketName The name of the bucket to grab the file from.
+ * @param fileKey The full path of the file that we want to get.
+ * @returns Information about the file including the file's body
+ */
+async function getFileFromS3(bucketName: string, fileKey: string) {
+  const client = createS3Client();
+  return await client.send(new GetObjectCommand({ Bucket: bucketName, Key: fileKey }));
+}
+
+/**
+ * Pipe a file body from S3 into a new file on disk.
+ *
+ * @param body The body as returned by `getFileFromS3`.
+ * @param fileName The name of the file that we want to create on disk.
+ * @returns A promise that resolves when the file has finished being written.
+ */
+function pipeBodyToFile(body: Readable | ReadableStream | Blob | undefined, fileName: string) {
+  if (!body) throw new Error("No body found");
+  if (!(body instanceof Readable)) throw new Error("Body isn't readable instance");
+
+  // As long as the body stream is readable, pipe it into a file!
+  return new Promise<void>((resolve, reject) => {
+    body
+      .pipe(fs.createWriteStream(fileName))
+      .on("error", (err) => reject(err))
+      .on("close", () => resolve());
+  });
+}
+
+//
+//  Exports
+//
+
+/**
+ * Downloads the desired emptystream Transmission source audio to a file on disk named "sourceAudio"
+ *
+ * @param bucketName The name of the bucket that contains all Transmissions
+ * @param id The ID of the Transmission to download the source audio for.
+ */
+export default async function downloadTransmissionSource(bucketName: string, id: string) {
+  // Start downloading the file from S3
+  const { Body } = await getFileFromS3(bucketName, `transmissions/${id}/sourceAudio`);
+
+  // Pipe the downloading file into a file on disk
+  await pipeBodyToFile(Body, "sourceAudio");
+}
--- containers/transmissionSplitter/src/main.ts
+++ containers/transmissionSplitter/src/main.ts
@@ -1,1 +1,3 @@
-console.log("Hello world!");
+// Read the ID of the transmission we want to split from the env vars
+const transmissionId: string = process.env["ID"] || "";
+if (!transmissionId) throw new Error("No transmission ID given");
